24aff1da94b8e8523247d8f07c51ac14916be5d3
dependable-coder-f3d7 <tj.han@yale.edu>
2021-06-01 23:13:11 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,9 +63,6 @@ def add_characters
     epic_tragedy[:additional_characters] << prince
     epic_tragedy[:additional_characters] << apothecary
 
-
-
-
    # After your code, the following line will return the altered hash
    epic_tragedy
 end
